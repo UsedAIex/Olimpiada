c25507e43c62c116220b5493f793d470963069e0
UsedAlex <miaf2289@gmail.com>
2024-02-03 16:09:40 +0300

    Переделал 3 задание, так как она не удовлетворяла условию

--- tsk3.py
+++ tsk3.py
@@ -1,14 +1,19 @@
 from helping_func import *
 
 
-def tsk_3(name_file: str, n_ship: str):
+def tsk_3(name_file: str):
     file = opening_file(name_file)
+    n_ship = input()
+    while n_ship != "stop":
+        for row in file:
+            if (row[0] + "-" + row[1]) == n_ship:
+                st = (f"Корабль {row[0]}-{row[1]} был отправлен с планеты: {row[2]} и "
+                      f"его направление движения было: {row[5]} {row[6]}")
 
-    for row in file:
-        if (row[0] + "-" + row[1]) == n_ship:
-            st = (f"Корабль {row[0]}-{row[1]} был отправлен с планеты: {row[2]} и "
-                  f"его направление движения было: {row[5]} {row[6]}")
+                print(st)
+                break
+        else:
+            print("error.. er.. ror..")
+        n_ship = input()
 
-            return st
-    else:
-        return "error.. er.. ror.."
+
